bdfc483dec0896174be4c7d47a874d183d7d77f7
irisida <20445648+irisida@users.noreply.github.com>
2021-03-28 17:45:41 +0100

    baseline for v2 of the app

---
+++ src/components/ViewList.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Calendar from './Calendar.svelte';
+  import List from './List.svelte';
+  import { fade } from 'svelte/transition';
+
+  let date = null;
+</script>
+
+<div in:fade>
+  <Calendar bind:date />
+  <List bind:date />
+</div>
---
+++ src/components/ViewObject.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Obj from './Obj.svelte';
+  import { fade } from 'svelte/transition';
+
+  export let objectId = null;
+</script>
+
+<div in:fade>
+  <Obj {objectId} />
+</div>
